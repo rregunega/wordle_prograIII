d7acd7b1f60edf70bf74b52da6b27ad36ece004c
Roberto Regunega <105944zz@gmail.com>
2026-03-28 17:01:01 -0300

    pruebas de seleccion de archivo con las palabras para luego mostrar una

--- src/juego/Principal.java
+++ src/juego/Principal.java
@@ -1,10 +1,12 @@
 package juego;
 
 public class Principal {
+	static lector_palabras palabras = new lector_palabras();
+	
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) {	
+	 System.out.println(palabras.devolverPalabra());
+		
 	}
 
 }
---
+++ src/juego/lector_palabras.java
@@ -0,0 +1,40 @@
+package juego;
+import java.io.*;
+import java.util.*;
+
+public class lector_palabras {
+	List<String> listaDePalabras=new ArrayList<>();
+	File palabras;
+	Scanner scan;
+	public lector_palabras()
+	{
+		inicializar();
+		leer();
+	}	
+	public void inicializar() {
+		try {
+			palabras=new File("src/res/palabras.txt");
+			scan= new Scanner(palabras);
+			}
+		
+		catch (FileNotFoundException e) {
+        System.out.println("¡Error! No encontré el archivo: " + e.getMessage());
+		}
+
+}
+	
+	public void leer() {
+	
+		while(scan.hasNextLine()) {
+			listaDePalabras.add(scan.nextLine());
+		}
+	}
+
+	public String devolverPalabra() {
+			int numero=(int) (this.listaDePalabras.size()-1*Math.random());
+			return listaDePalabras.get(numero);
+			
+		}
+
+
+}
