b12d6e5a50ad384659b2650f5892289a76a3bd76
Roberto Regunega <105944zz@gmail.com>
2026-04-02 20:21:49 -0300

    eliminado el acoplamiento en la funcion verificarLetra

---
+++ src/juego/Estado.java
@@ -0,0 +1,7 @@
+package juego;
+
+public class Estado {
+	public static final String Verde="verde";
+	public static final String amarillo="amarillo";
+	public static final String gris="gris";
+}
--- src/juego/Principal.java
+++ src/juego/Principal.java
@@ -7,62 +7,76 @@ public class Principal {
 	static lector_palabras palabras = new lector_palabras();
 	
 	public static void perderJuego() {
-		
 		System.out.println("perdio");
 	}
-public static void ganoJuego() {
-		gano=true;
-		System.out.println("gano");
+	
+	
+	public static void ganoJuego() {
+			gano=true;
+			System.out.println("gano");
 	}
 	
 	
-	public static void verificarLetra(String PalabraUsuario,String PalabraMaquina ){
-		System.out.println(PalabraMaquina);
-		if(PalabraMaquina.toLowerCase().equals(PalabraUsuario)) {
-			ganoJuego();
-			return;}
-		
+	public static String[] verificarLetra(String PalabraUsuario,String PalabraMaquina ){
+		String[] estados= new String[5]; 
+
 		for(int indice=0; indice<PalabraUsuario.length(); indice++) {
 			
 			char usuario=PalabraUsuario.charAt(indice);
-			char maquina=PalabraMaquina.toLowerCase().charAt(indice);
+			char maquina=PalabraMaquina.charAt(indice);
 			
-			if(PalabraMaquina.toLowerCase().contains(String.valueOf(usuario))) {
-				
-			if(usuario==maquina) { mostrarLetras.mostrarVerde(usuario);
-			continue;}
-			else {mostrarLetras.mostrarAmarillo(usuario);
-			continue;}}
+			if(PalabraMaquina.toLowerCase().contains(String.valueOf(usuario))) {	
+			if(usuario==maquina) {
+				estados[indice]=Estado.Verde;
+				continue;}
+			else {	
+				estados[indice]=Estado.amarillo;
+				continue;}
+			}
 			
-			mostrarLetras.mostrarGris(usuario);
+			estados[indice]=Estado.gris;;
 			continue;
 				
-		}    
 		}
+		
+		return estados;
+		}
+	
+	private static void mostrarLetras(String entrada, String[] estado) {
+		for(int indice=0; indice<entrada.length(); indice++) {
+			System.out.println(entrada.charAt(indice)+ " se muestra en = "+ estado[indice]+"\n");
+		}
+	}
 	
 
 
 	public static void main(String[] args) {
 		
 	
-	String palabra=palabras.devolverPalabra();
+	String palabra=palabras.devolverPalabra().toLowerCase();
 	String entrada;
 	Scanner sc=new Scanner(System.in);
 	
 	while(intentos>0 && !gano) {
 		System.out.println("ingrese una palabra de cinco letras maximo");
-		entrada=sc.nextLine();
+		entrada=sc.nextLine().toLowerCase();
 		
 		if(entrada.length()>5 || entrada.length()<1) {
 			System.out.println("palabra invalida (muy larga o muy corta\n recuerde que debe ser de 5 letras maximo)");
 			continue;
 		}
+		
 		modificadores.rellenarPalabra(entrada);
-		verificarLetra(entrada,palabra);
+		
+		if(palabra.toLowerCase().equals(entrada)) {
+			ganoJuego();
+			return;}
+		mostrarLetras(entrada,verificarLetra(entrada,palabra));
 		intentos-=1;
 	}
 	if(intentos==0) {perderJuego();}
 	
 	}
+
 
 }
