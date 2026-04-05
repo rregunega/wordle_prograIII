c62ff5bf08202e95ed33a5b010327da809490685
CristianBrz <cristian.brz.83@gmail.com>
2026-04-05 02:26:48 -0300

    Agrego clase para configurar estilos visuales

---
+++ src/gui/ConfiguracionUI.java
@@ -0,0 +1,34 @@
+package gui;
+
+import java.awt.Color;
+import java.awt.Font;
+
+public class ConfiguracionUI {
+	// Colores
+	public static final Color COLO_PRIMARIO = new Color(59, 60, 61);
+	public static final Color COLOR_PRIMARIO_CLARO = new Color(212, 234, 247);
+
+	public static final Color COLOR_ACENTO = new Color(0, 102, 140);
+	public static final Color COLOR_ACENTO_CLARO = new Color(113, 196, 239);
+
+	public static final Color COLOR_FONDO_PRIMARIO = new Color(255, 254, 251);
+	public static final Color COLOR_FONDO_SECUNDARIO = new Color(250, 244, 241);
+
+	public static final Color COLOR_TEXTO_OSCURO = new Color(30, 31, 34);
+	public static final Color COLOR_TEXTO_CLARO = new Color(255, 253, 251);
+
+	public static final Color AZUL = new Color(13, 110, 253);
+	public static final Color GRIS = new Color(108, 117, 125);
+	public static final Color VERDE = new Color(25, 135, 84);
+	public static final Color ROJO = new Color(220, 53, 69);
+	public static final Color AMARILLO = new Color(255, 193, 7);
+	public static final Color CYAN = new Color(13, 202, 240);
+	public static final Color CLARO = new Color(248, 249, 250);
+	public static final Color OSCURO = new Color(33, 37, 41);
+
+	// Fuentes
+	public static final Font FUENTE_TITULO = new Font("Segoe UI", Font.BOLD, 24);
+	public static final Font FUENTE_SUBTITULO = new Font("Segoe UI", Font.BOLD, 20);
+	public static final Font FUENTE_TEXTO = new Font("Segoe UI", Font.PLAIN, 16);
+	public static final Font FUENTE_BOTON = new Font("Segoe UI", Font.BOLD, 18);
+}
