8a2c0bc5e90c94631b5621d2788c99832f1ed9c4
rregunega <105944zz@gmail.com>
2026-05-19 19:26:04 -0300

    .

--- src/juego/Partida.java
+++ src/juego/Partida.java
@@ -16,10 +16,6 @@ public class Partida {
 		return this.palabra;
 	}
 
-	public void intento() {
-
-		
-	}
 
 	public Letra[] verificarLetra(String PalabraUsuario) {
 		String PalabraMaquina=this.palabra;
@@ -68,20 +64,6 @@ public class Partida {
 		return false;
 	}
 
-	
-	private static void rellenarPalabra(String entrada) {
-
-		if (entrada.length() < 5) {
-			StringBuilder salida = new StringBuilder(entrada);
-
-			int numero_relleno = 5 - entrada.length();
-
-			for (int i = 0; i < numero_relleno; i++) {
-				salida.append(" ");
-			}
-			entrada = salida.toString();
-		}
-	}
 
 	public boolean gano() {
 		return gano;
